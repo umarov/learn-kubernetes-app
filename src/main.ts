559d9524960d52b7adae802c2a904964065bac7e
Muzafar Umarov <umarov@gmail.com>
2020-10-04 16:01:13 -0400

    Working version with local cluster:

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,15 @@ const port = process.env.PORT && +process.env.PORT || 8080
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ logger: true })
+    new FastifyAdapter({
+      logger: true, rewriteUrl: (req) => {
+        if (req.url.startsWith('/learn-kubernetes-app')) {
+          return req.url.replace('/learn-kubernetes-app', '')
+        } else {
+          return req.url
+        }
+      }
+    })
   );
   await app.listen(port, host);
 }
