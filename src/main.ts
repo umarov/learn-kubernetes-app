cbb0566f82c5d79f53f8e13266b65ac9ddb4f7fb
Muzafar Umarov <umarov@gmail.com>
2020-10-04 14:24:26 -0400

    Add fastify because no need for express

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 const host = process.env.APP_HOST || '0.0.0.0'
 const port = process.env.PORT && +process.env.PORT || 8080
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true })
+  );
   await app.listen(port, host);
 }
+
 bootstrap();
